c043a288a92d5c53eb886f0f99d0b76c5a5aabef
williamchh <williamchh@gmail.com>
2024-11-04 23:04:11 +1030

    Add privacy policy page and update manifest permissions

--- src/manifest.ts
+++ src/manifest.ts
@@ -42,7 +42,7 @@ export default defineManifest({
       matches: ["<all_urls>"],
     },
   ],
-  permissions: ['storage', 'tabs', 'activeTab', 'scripting', 'offscreen'],
+  permissions: ['storage', 'activeTab', 'scripting', 'offscreen'],
   host_permissions: [
     'http://*/*',
     'https://*/*',
